e07de8a781750a2a0e3cfcca0ed19ac14a399182
Szymon Borowy <szymonborowy@wp.pl>
2018-12-01 12:01:01 +0100

    Two method for create new Thread

---
+++ src/main/java/core/multithreading/HelloWithExtends.java
@@ -0,0 +1,15 @@
+package core.multithreading;
+
+public class HelloWithExtends extends Thread{
+    public static void main(String[] args) throws InterruptedException {
+        Thread t = new Thread();
+        t.start();
+        Thread.sleep(1);
+        System.out.println("Hello World");
+    }
+
+    @Override
+    public void run() {
+        System.out.println("Hello I'm thread");
+    }
+}
---
+++ src/main/java/core/multithreading/HelloWithImplements.java
@@ -0,0 +1,15 @@
+package core.multithreading;
+
+public class HelloWithImplements implements Runnable {
+    public static void main(String[] args) throws InterruptedException {
+        Thread t = new Thread(new HelloWithImplements());
+        t.start();
+        Thread.sleep(1);
+        System.out.println("Hello World");
+    }
+
+    @Override
+    public void run() {
+        System.out.println("Hello I'm thread");
+    }
+}
