09d5aa3138649023b479bd06b00487746d4d3200
Szymon Borowy <szymonborowy@wp.pl>
2018-12-01 12:12:26 +0100

    Two methods for stop thread, but not good

---
+++ src/main/java/core/multithreading/StopThread.java
@@ -0,0 +1,19 @@
+package core.multithreading;
+
+public class StopThread {
+    public static void main(String[] args) throws InterruptedException {
+        Thread t = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                int i = 0;
+                while (true) {
+                    i++;
+                    System.out.println(i);
+                }
+            }
+        });
+        t.start();
+        Thread.sleep(5000);
+        t.stop();
+    }
+}
---
+++ src/main/java/core/multithreading/StopThread2.java
@@ -0,0 +1,26 @@
+package core.multithreading;
+
+public class StopThread2 {
+    private static boolean stopRequested = false;
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread t = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("Uruchomiono nowy wątek");
+                int i = 0;
+                while (!stopRequested) {
+                    i++;
+                    System.out.println(i);
+                }
+                System.out.println("Zakończono drugi wątek");
+            }
+        });
+
+        System.out.println("Uruchomiono główny wątek");
+        t.start();
+        Thread.sleep(500);
+        stopRequested = true;
+        System.out.println("Zakończono główny wątek");
+    }
+}
