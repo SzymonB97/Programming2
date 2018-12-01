8b4a08c4cd3267c55f1f79f034445ae98fa14e05
Szymon Borowy <szymonborowy@wp.pl>
2018-12-01 15:02:38 +0100

    Mockito test for HanselAndGretel

---
+++ src/test/java/core/HanselAndGretelTest.java
@@ -0,0 +1,26 @@
+package core;
+
+import core.multithreading.HanselAndGretel;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import java.io.PrintStream;
+
+public class HanselAndGretelTest {
+
+    @Test
+    void shouldDoTasks() throws InterruptedException {
+        //ustawiamy naszego mock'a jako symulację klasy PrintStream
+        PrintStream mock = Mockito.mock(PrintStream.class);
+
+        //ustawiamy metodę wywołania teksu na naszego mock'a
+        System.setOut(mock);
+
+        //uruchamiamy main naszej klasy
+        HanselAndGretel.main(null);
+
+        //sprawdzamy czy nasz mock wypisał dany tekst
+        Mockito.verify(mock).println("Jaś skończył się ubierać");
+    }
+
+}
