b24b9bd8954a6ac4cf69fc9b692557d908498637
Szymon Borowy <szymonborowy@wp.pl>
2018-12-02 11:57:42 +0100

    Comparison between calculate by one thread and five threads

--- src/main/java/core/multithreading/ThreadRunner.java
+++ src/main/java/core/multithreading/ThreadRunner.java
@@ -6,6 +6,7 @@ import java.util.concurrent.*;
 
 public class ThreadRunner implements Callable<String> {
     private static final long MAX_VALUE = 100_000_000_000L;
+    private static final int NUMBER_OF_THREADS = 5;
 
     public static void main(String[] args) throws InterruptedException, ExecutionException {
         execute();
@@ -13,9 +14,9 @@ public class ThreadRunner implements Callable<String> {
     }
 
     private static void multiExecute() throws InterruptedException, ExecutionException {
-        ExecutorService executor = Executors.newFixedThreadPool(5);
+        ExecutorService executor = Executors.newFixedThreadPool(NUMBER_OF_THREADS);
         List<Callable<String>> list = new ArrayList<>();
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < NUMBER_OF_THREADS; i++) {
             list.add(new ThreadRunner());
         }
 
@@ -36,7 +37,7 @@ public class ThreadRunner implements Callable<String> {
     @Override
     public String call() throws Exception {
         long startTime = System.nanoTime();
-        for (long i = 0; i < MAX_VALUE / 5; i++) ;
+        for (long i = 0; i < MAX_VALUE /NUMBER_OF_THREADS; i++) ;
         long endTime = System.nanoTime();
         return String.format("%s: %s", Thread.currentThread().getName(), String.valueOf((double) (endTime - startTime) / 1_000_000_000));
     }
