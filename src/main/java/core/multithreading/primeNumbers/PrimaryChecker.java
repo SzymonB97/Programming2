b7c1cc602b1f228ae1ea3e0f6207ea77754712c3
Szymon Borowy <szymonborowy@wp.pl>
2018-12-02 14:24:04 +0100

    PrimaryChecker by one thread

--- src/main/java/core/multithreading/primeNumbers/PrimaryChecker.java
+++ src/main/java/core/multithreading/primeNumbers/PrimaryChecker.java
@@ -1,19 +1,41 @@
 package core.multithreading.primeNumbers;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class PrimaryChecker {
+    private static final String FILE_PATH1 = "D:\\Projekty Java\\Programming2\\src\\main\\java\\core\\multithreading\\primeNumbers\\numbers.txt";
+    private static final String FILE_PATH2 = "D:\\Projekty Java\\Programming2\\src\\main\\java\\core\\multithreading\\primeNumbers\\primes-to-1000k.txt";
+
     public static void main(String[] args) {
-        System.out.println(isPrime(10));
+        long startTime = System.nanoTime();
+        System.out.println("Ilośc liczb pierwszych w pliku: " + getAndCheckNumbers());
+        long endTime = System.nanoTime();
+        System.out.println("Czas wykonania: " + ((endTime - startTime)/1_000_000 + " ms"));
+    }
+
+    private static int getAndCheckNumbers() {
+        int primeCounter = 0;
+        try (Scanner scanner = new Scanner(new File(FILE_PATH1))){
+            while (scanner.hasNextLong()) {
+                if (isPrime(scanner.nextInt())) primeCounter++;
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        return primeCounter;
     }
 
     public static boolean isPrime(long number) {
         if (number < 2) return false;
+        if (number == 2) return true;
 
-        int counter = 0;
-        for (int i = 2; i < number; i++) {
-            if (counter > 2) return false;
-            if (number % (i - 1) == 0) {
-                counter++;
-            }
+        for (long i = 2; i <= Math.sqrt(number); i++) {
+            //wystarczy tylko takie sprawdzenie ponieważ zaczynamy od 2, więc mamy od razu sprawdzoną 2 i 1
+            //wystarczy jeden dzielnik więcej aby stwierdzić, że liczba nie jest pierwsza
+            if (number % (i) == 0) return false;
         }
 
         return true;
