68c159d2187f608a8072c7de7d50e37170f8fca6
Szymon Borowy <szymonborowy@wp.pl>
2018-12-09 15:41:12 +0100

    Swing example with using designer

--- src/main/java/core/swing/SwingHelloWorldWindow.java
+++ src/main/java/core/swing/SwingHelloWorldWindow.java
@@ -8,11 +8,14 @@ public class SwingHelloWorldWindow {
 
     public static void main(String[] args) {
         JFrame frame = new JFrame("SwingHelloWorldWindow");
-
         //połączenie z designer'em
         frame.setContentPane(new SwingHelloWorldWindow().mainPanel);
 
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        //rozmieszczenie okna na ekranie
+        frame.setBounds(300, 300,frame.getWidth(),frame.getHeight());
+
         frame.pack();
         frame.setVisible(true);
     }
