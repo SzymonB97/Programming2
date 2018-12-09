6adeffcb541d7b026e96812af2f2d132421264d2
Szymon Borowy <szymonborowy@wp.pl>
2018-12-09 10:00:35 +0100

    Language detector

--- src/main/java/core/languageChecker/LanguageDetector.java
+++ src/main/java/core/languageChecker/LanguageDetector.java
@@ -4,21 +4,52 @@ import com.detectlanguage.DetectLanguage;
 import com.detectlanguage.Result;
 import com.detectlanguage.errors.APIError;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
 
 public class LanguageDetector {
-    public static void main(String[] args) throws APIError {
-        String languageEn = detect("Hello World");
-        String languagePl = detect("Witaj świecie");
 
-        System.out.println("Rozpoznany język: " + languageEn);
-        System.out.println("Rozpoznany język: " + languagePl);
+    private static final String PATH_FOLDER = "D:\\Projekty Java\\Programming2\\src\\main\\java\\core\\languageChecker\\input";
+
+    public static void main(String[] args) throws APIError, IOException {
+        List<String> filePaths = readAllStringFiles(PATH_FOLDER);
+
+        for (String filePath : filePaths) {
+            String fileLines = readTextFromFile(filePath);
+            String language = detect(fileLines);
+            System.out.println("Rozpoznany język: " + language);
+        }
     }
 
-    private static String detect(String word) throws APIError {
+    //przekazujemy listą ścieżki do pliku
+    private static List<String> readAllStringFiles(String path) {
+        File folder = new File(path);
+        File[] filesList = folder.listFiles();
+        List<String> textList = new ArrayList<>();
+
+        for (File file : filesList) {
+            if (file.isFile()) {
+                textList.add(file.getAbsolutePath());
+            }
+        }
+
+        return textList;
+    }
+
+    //odczytujemy przekazany plik
+    private static String readTextFromFile(String filePath) throws IOException {
+        return new String(Files.readAllBytes(Paths.get(filePath)));
+    }
+
+    //sprawdzamy język
+    private static String detect(String words) throws APIError {
         DetectLanguage.apiKey = "4f8cc8f4e86081b0636fcd5336f8ed1c";
         DetectLanguage.ssl = true;
-        List<Result> results = DetectLanguage.detect(word);
+        List<Result> results = DetectLanguage.detect(words);
 
         return results.get(0).language;
     }
